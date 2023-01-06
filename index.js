fc6fd8f312f7b849668037e210dba2107be5fe6d
Mihai Smarandache <mihai.sanfran@gmail.com>
2023-01-05 18:13:02 -0700

    Remove agentsRouter

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 //import express from "express";
 const express = require("express");
 //import router from "./routers/agents";
-const agentsRouter = require("./routers/agents");
+//const agentsRouter = require("./routers/agents");
 const app = express();
 
 // Automatically parse data as a JSON object in all request handlers
 app.use(express.json());
 
 //app.use(propertiesRouter);
-app.use(agentsRouter);
+//app.use(agentsRouter);
 
 app.listen(5001, () => console.log("Api running on port 5001"));
 
-app.get("/", (req, res) => res.json("My API running! 7778"));
+app.get("/", (req, res) => res.json("My API running! 77789"));
