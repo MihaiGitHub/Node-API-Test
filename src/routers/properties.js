b499db2944dbc8c66cab6488eb173cb7033e37c4
Mihai Smarandache <mihai.sanfran@gmail.com>
2023-01-05 17:27:47 -0700

    Inde file

--- src/routers/properties.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = new express.Router();
-const { list, search } = require("../controllers/properties");
-
-router.get("/properties", list);
-router.get("/search/properties", search);
-
-module.exports = router;
