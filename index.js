75aa400fb65d6e21fb4156b6f2369d8e9f97d2ef
Mihai Smarandache <mihai.sanfran@gmail.com>
2023-01-05 17:39:19 -0700

    Testing

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const app = express();
 
 app.listen(5001, () => console.log("Api running on port 5001"));
 
-app.get("/", (req, res) => res.json("My API running!"));
+app.get("/", (req, res) => res.json("My API running! 777"));
