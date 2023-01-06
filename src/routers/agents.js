cdd09e3801be91ca450c82d2209148576f29283a
Mihai Smarandache <mihai.sanfran@gmail.com>
2023-01-05 18:50:56 -0700

    Comment out agents router

--- src/routers/agents.js
+++ src/routers/agents.js
@@ -1,19 +1,19 @@
-const express = require("express");
-//import express from "express";
-const router = new express.Router();
-const {
-  //agentById, read,
-  list,
-  //saveMessage
-} = require("../controllers/agent");
+// const express = require("express");
+// //import express from "express";
+// const router = new express.Router();
+// const {
+//   //agentById, read,
+//   list,
+//   //saveMessage
+// } = require("../controllers/agent");
 
-// middleware
-// anytime there is a userId param in the route execute agentById method
-//router.param("agentId", agentById);
+// // middleware
+// // anytime there is a userId param in the route execute agentById method
+// //router.param("agentId", agentById);
 
-// routes
-//router.post("/agent/:agentId/message", saveMessage);
-//router.get("/agent/:agentId", read);
-router.get("/agents", list);
+// // routes
+// //router.post("/agent/:agentId/message", saveMessage);
+// //router.get("/agent/:agentId", read);
+// router.get("/agents", list);
 
-module.exports = router;
+// module.exports = router;
