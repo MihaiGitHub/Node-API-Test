94890e2d7cca89a27a1ad1f0f72c7cc4c102e652
Mihai Smarandache <mihai.sanfran@gmail.com>
2023-01-08 18:41:07 -0700

    Send back json

--- index.js
+++ index.js
@@ -30,4 +30,8 @@ app.use(express.json());
 
 app.listen(5001, () => console.log("Api running on port 5001"));
 
-app.get("/", (req, res) => res.json("My API running! 77789"));
+app.get("/", (req, res) =>
+  res.json({
+    message: "Real estate API running!",
+  })
+);
