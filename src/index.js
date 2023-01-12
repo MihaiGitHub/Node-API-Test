4b770c6c7a457f56a62d993c7edaf7d4266716a8
Mihai Smarandache <mihai.sanfran@gmail.com>
2023-01-11 19:00:17 -0700

    Refactor code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 //import express from "express";
 const express = require("express");
 //import router from "./routers/agents";
-//const agentsRouter = require("./routers/agents");
+const agentsRouter = require("./routers/agents");
 const app = express();
 
 ///////////////////////////////////////////
+/*
 //const router = new express.Router();
 const {
   //agentById, read,
@@ -20,13 +21,14 @@ const {
 //router.post("/agent/:agentId/message", saveMessage);
 //router.get("/agent/:agentId", read);
 app.get("/agents", list);
+*/
 ////////////////////////////////////////////////
 
 // Automatically parse data as a JSON object in all request handlers
 app.use(express.json());
 
 //app.use(propertiesRouter);
-//app.use(agentsRouter);
+app.use(agentsRouter);
 
 app.listen(5001, () => console.log("Api running on port 5001", process.env));
 
