3c492dde340158aa3203608552c13efd48a5c8d4
Mihai Smarandache <mihai.sanfran@gmail.com>
2023-01-13 16:02:44 -0700

    Add properties router to main file

--- src/controllers/properties.js
+++ src/controllers/properties.js
@@ -1,18 +1,7 @@
 const Property = require("../models/property");
 const PropertyImages = require("../models/property_images");
 const User = require("../models/user");
-/*
-const { Sequelize, Model, DataTypes } = require("sequelize");
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-  }
-);
-*/
+
 exports.list = async (req, res) => {
   try {
     const properties = await Property.findAll({
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const agentsRouter = require("./routers/agents");
+const propertiesRouter = require("./routers/properties");
 const app = express();
 
 // Automatically parse data as a JSON object in all request handlers
 app.use(express.json());
 
-//app.use(propertiesRouter);
+app.use(propertiesRouter);
 app.use(agentsRouter);
 
 app.listen(5001, () => console.log("Api running on port 5001"));
