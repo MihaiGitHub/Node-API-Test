c307d8af7ff53c23d038fec783d67d24641d3271
Mihai Smarandache <mihai.sanfran@gmail.com>
2023-01-14 15:00:49 -0700

    Update models

--- src/models/message.js
+++ src/models/message.js
@@ -1,17 +1,13 @@
 const { Sequelize, Model, DataTypes } = require("sequelize");
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USERNAME,
-//   process.env.DB_PASSWORD,
-//   {
-//     host: process.env.DB_HOST,
-//     dialect: "mysql",
-//   }
-// );
-const sequelize = new Sequelize("realestate", "root", "MyNewPass1!", {
-  host: "localhost",
-  dialect: "mysql",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  }
+);
 
 const Message = sequelize.define("messages", {
   uid: {
